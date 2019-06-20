8746528aa55b6ff08b5be2331c5bd4cecf586560
kappz <addcolor117@gmail.com>
2019-06-20 17:43:16 -0400

    created model for movie, dbcontext for movie model, verified CRUD functionality for dbcontext.

--- MvcMovie/MvcMovie/Startup.cs
+++ MvcMovie/MvcMovie/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie {
     public class Startup {
@@ -26,8 +28,13 @@ namespace MvcMovie {
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+    //services.AddDbContext<MvcMovieContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
